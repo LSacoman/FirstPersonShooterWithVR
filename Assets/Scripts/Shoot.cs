3b4beb0e1e45b53e5fda19b43374234ffa1c7b72
Leonardo Sacoman <leonardodentz@hotmail.com>
2018-10-24 20:05:14 -0300

    first commit

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+
+    public GameObject bulletSpawn;
+
+    private bool isShooting;
+	// Use this for initialization
+	void Start () {
+        isShooting= false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hit;
+        Debug.DrawRay(bulletSpawn.transform.position, bulletSpawn.transform.forward, Color.green);
+
+        if(Physics.Raycast(bulletSpawn.transform.position, bulletSpawn.transform.forward, out hit, 100)){
+            if (hit.collider.name.Contains("zombie")){
+                if (!isShooting)
+                {
+                    StartCoroutine("Fire");
+                }
+            }
+        }
+    }
+
+    IEnumerator Fire(){
+        isShooting = true;
+        GameObject bullet = Instantiate(Resources.Load("bullet", typeof(GameObject))) as GameObject;
+        bullet.transform.position = bulletSpawn.transform.position;
+        bullet.transform.rotation = bulletSpawn.transform.rotation;
+
+        Rigidbody rb = bullet.GetComponent<Rigidbody>();
+        rb.AddForce(bulletSpawn.transform.forward * 500f);
+        GetComponent<AudioSource>().Play();
+        GetComponent<Animation>().Play();
+
+        Destroy(bullet, 1f);
+
+        yield return new WaitForSeconds(1f);
+        isShooting = false;
+    }
+}
---
+++ Assets/Scripts/ZombieWalk.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZombieWalk : MonoBehaviour {
+
+    private Transform target;
+    private NavMeshAgent agent;
+
+	void Start () {
+        target = Camera.main.transform;
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = target.position;
+        GetComponent<Animation>().Play("walk");		
+	}
+	
+
+    void OnTriggerEnter(Collider other)
+    {
+        GetComponent<SphereCollider>().enabled = false;
+        Destroy(other.gameObject);
+
+        agent.destination = gameObject.transform.position;
+        GetComponent<Animation>().Stop();
+        GetComponent<Animation>().Play("back_fall");
+        Destroy(gameObject, 6f);
+        GameObject zombie = Instantiate(Resources.Load("zombie", typeof(GameObject))) as GameObject;
+        float randomX = UnityEngine.Random.Range(-12, 12);
+        float randomZ = UnityEngine.Random.Range(-12, 12);
+
+        zombie.transform.position = new Vector3(randomX, 0.1f, randomZ);
+
+        while(Vector3.Distance(zombie.transform.position, target.position) <= 3)
+        {
+            randomX = UnityEngine.Random.Range(-12, 12);
+            randomZ = UnityEngine.Random.Range(-12, 12);
+
+            zombie.transform.position = new Vector3(randomX, 0.1f, randomZ);
+        }
+    }
+}
